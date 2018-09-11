f5ca0c5cb68bfe6430d8f0afe2fa968829b68b7e
DarooriGopi <gpkrishna23@gmail.com>
2018-09-11 17:21:36 -0400

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ConfirmationComponentComponent } from './confirmation-component/confirmation-component.component';
+import { FromToComponentComponent } from './from-to-component/from-to-component.component';
+import { EnterAmountAndReviewComponentComponent } from './enter-amount-and-review-component/enter-amount-and-review-component.component';
+
+const routes: Routes = [
+  {path: '', component: FromToComponentComponent },
+  {path: 'enter-amount-and-review-component', component: EnterAmountAndReviewComponentComponent},
+  {path: 'from-to-component', component: FromToComponentComponent},
+  {path: 'confirmation-component', component: ConfirmationComponentComponent}
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
