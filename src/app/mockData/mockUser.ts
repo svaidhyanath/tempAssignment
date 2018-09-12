7d711e76491a026782659511f0ee27cdfe2536d5
Sainath Vaidhyanath <vaidhyanath.sainath@gmail.com>
2018-09-12 01:38:55 -0500

    in prog

--- src/app/mockData/mockUser.ts
+++ src/app/mockData/mockUser.ts
@@ -2,5 +2,5 @@ import { MockUserObject } from '../models/mock-user-object';
 
 export const MOCK_SAMPLE_USER: MockUserObject = {
   userIdentifier: 10001,
-  name: 'John Doe'
+  name: 'James Smith'
 };
