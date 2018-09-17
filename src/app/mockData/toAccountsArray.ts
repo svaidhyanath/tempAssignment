b91fea767abd96b6a4aa716f70edd910dfeb8e7e
Sainath Vaidhyanath <vaidhyanath.sainath@gmail.com>
2018-09-17 15:48:59 -0500

    temp

--- src/app/mockData/toAccountsArray.ts
+++ src/app/mockData/toAccountsArray.ts
@@ -6,34 +6,55 @@ export const MOCK_TO_ACCOUNTS_ARRAY:  MockToAccountsModel[] = [
     accountNumber:  '4545454545',
     accountName:  'Shane Beiber',
     amountBal: '214.59',
-    isSelected:  false
+    isSelected:  false,
+    imageFile: 'shane-66x66.png'
   },
   {
     accountType:  'Payee',
     accountNumber:  '9999999990',
     accountName:  'Alex Jones',
     amountBal: '1234.56',
-    isSelected:  false
+    isSelected:  false,
+    imageFile: 'alex-66x66.png'
   },
   {
     accountType:  'Payee',
     accountNumber:  '1357986420',
     accountName:  'Baker Mayfield',
     amountBal: '98.76',
-    isSelected:  false
+    isSelected:  false,
+    imageFile: 'shane-80x80.png'
   },
   {
     accountType:  'Payee',
     accountNumber:  '1357986420',
     accountName:  'Bing Zhang',
     amountBal: '6264.69',
-    isSelected:  false
+    isSelected:  false,
+    imageFile: 'bing-80x80.png'
   },
   {
     accountType:  'Payee',
     accountNumber:  '1357986420',
     accountName:  'Theo Riddick',
     amountBal: '33214.99',
-    isSelected:  false
+    isSelected:  false,
+    imageFile: 'shane-80x80.png'
+  },
+  {
+    accountType:  'Payee',
+    accountNumber:  '1357986420',
+    accountName:  'Gavin Johnson',
+    amountBal: '1498.22',
+    isSelected:  false,
+    imageFile: 'shane-80x80.png'
+  },
+  {
+    accountType:  'Payee',
+    accountNumber:  '1357986420',
+    accountName:  'Gas Co.',
+    amountBal: '98.76',
+    isSelected:  false,
+    imageFile: 'shane-80x80.png'
   }
 ];
--- src/app/models/mock-to-accounts-model.ts
+++ src/app/models/mock-to-accounts-model.ts
@@ -4,4 +4,5 @@ export class MockToAccountsModel  {
   accountName: String;
   isSelected: Boolean;
   amountBal: String;
+  imageFile: String;
 }
